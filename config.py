260d5a97bf2e817173f48cc4bc5d694c9c71dca2
Surajsurya95096 <surajroxofficial01@gmail.com>
2025-04-15 09:59:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ id_pattern = re.compile(r'^.\d+$')
 class Rkn_Bots(object):
     
     # Rkn client config  ( required.. 😥)
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = os.environ.get("API_ID", "26992030")
+    API_HASH = os.environ.get("API_HASH", "4da7d71c6bc4512a886e41aca83a5ee3")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7748143534:AAGtLr_s2JqTEWyt4KQJuQDE3oKMPM3VpuE")
 
     # start_pic
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/21a8e96b45cd6ac4d3da6.jpg")
@@ -22,21 +22,21 @@ class Rkn_Bots(object):
     PORT = int(os.environ.get("PORT", "8080"))
 
     # force subs channel ( required.. 😥)
-    FORCE_SUB = os.environ.get("FORCE_SUB", "Rkn_Botz") 
+    FORCE_SUB = os.environ.get("FORCE_SUB", "-1002456267845 -1002399683412") 
     
     # database config ( required.. 😥)
-    DB_NAME = os.environ.get("DB_NAME", "AutoCaption_V05_Bot")     
-    DB_URL = os.environ.get("DB_URL", "")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")     
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://vishwajitkr642006:BEPMAOkwnBCBxeHd@cluster0.c97un.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     # default caption 
-    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https//:t.me/Rkn_Botz'>{file_name} Main Telegram Channel: @RknDeveloper</a></b>",
+    DEF_CAP = os.environ.get("DEF_CAP", "<b><a href='https://t.me/Deendayal_Hindi_Movie'>{file_name} Main Telegram Channel: @Deendayal_Hindi_Movie</a></b>",
     )
 
     # sticker Id
     STICKER_ID = os.environ.get("STICKER_ID", "CAACAgIAAxkBAAELFqBllhB70i13m-woXeIWDXU6BD2j7wAC9gcAAkb7rAR7xdjVOS5ziTQE")
 
     # admin id  ( required.. 😥)
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7474015499').split()]
     
 
 # Rkn Developer 
